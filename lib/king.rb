e21067aaad0914ad5ff9cabcecc54c7dd1602083
ZarathustraSama <mr.gabriel@live.it>
2024-01-18 12:16:48 +0100

    Initial commit

---
+++ lib/king.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative './piece'
+
+class King<Piece
+  attr_accessor :moves
+
+  def initialize
+    super
+    @moves = [-1, 1].map { |elem| [0, elem] } + [-1, 1].map { |elem| [elem, 0] } +
+              [-1, 1].map { |elem| [elem, elem] } + [-1, 1].map { |elem| [elem, -elem] }
+  end
+end
