ccccb23fe9acb162eecef91de120972b4ce2a9ef
ZarathustraSama <mr.gabriel@live.it>
2024-01-19 11:34:26 +0100

    Moved check/mate/stalemate logic to Board

--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
 class Game
-  def check?(board)
 
-  end
-
-  def game_over?(board)
-    mate? || stalemate?
-  end
-
-  def mate?(board)
-
-  end
-
-  def stalemate?(board)
-
-  end
 end
