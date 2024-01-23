e7594347fa720285dd87e02a1065efa26b84cc54
ZarathustraSama <mr.gabriel@live.it>
2024-01-23 10:10:38 +0100

    Added #promotable?

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -2,14 +2,19 @@
 
 require_relative './piece'
 
-class Pawn<Piece
+# The pawn piece
+class Pawn < Piece
   attr_accessor :move, :s_move, :capture_move
 
   def initialize(*args)
     super
-    @normal_move = self.color == 'White' ? [-1, 0] : [1, 0]
-    @super_move = self.color == 'White' ? [-2, 0] : [2, 0]
+    @normal_move = @color == 'White' ? [-1, 0] : [1, 0]
+    @super_move = @color == 'White' ? [-2, 0] : [2, 0]
     # For simplicity it has the same name of the other pieces
-    @moves = self.color == 'White' ? [[-1, -1], [-1, 1]] : [[1, 1], [1, -1]]
+    @moves = @color == 'White' ? [[-1, -1], [-1, 1]] : [[1, 1], [1, -1]]
+  end
+
+  def promotable?
+    (@color == 'White' && @position[0].zero?) || (@color == 'Black' && @position[0] == 7)
   end
 end
