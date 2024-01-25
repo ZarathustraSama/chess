d3f4484ad293b540f628677b1b5bf60f48c0a8ae
ZarathustraSama <mr.gabriel@live.it>
2024-01-25 10:02:26 +0100

    Reworked/moved some functions

--- lib/board.rb
+++ lib/board.rb
@@ -8,8 +8,26 @@ require_relative './queen'
 require_relative './king'
 
 EMPTY = nil
+WHITE = 'White'
+BLACK = 'Black'
+
+PIECES = {
+  w_king: '\u2654',
+  w_queen: '\u2655',
+  w_rook: '\u2656',
+  w_bishop: '\u2657',
+  w_knight: '\u2658',
+  w_pawn: '\u2659',
+  b_king: '\u265A',
+  b_queen: '\u265B',
+  b_rook: '\u265C',
+  b_bishop: '\u265D',
+  b_knight: '\u265E',
+  b_pawn: '\u265F'
+}.freeze
 
 # The chess board
+# Every component that only "snapshots" the board's status, is here
 class Board
   attr_accessor :board
 
@@ -26,20 +44,21 @@ class Board
     add_pawns
   end
 
-  def check?(enemy, ally)
-    simulate_all_positions(find_same_pieces(enemy)).include?(find_king(ally))
+  def game_over?(color)
+    mate?(color) || stalemate?(color)
   end
 
-  def game_over?(enemy, ally)
-    mate?(enemy, ally) || stalemate?(enemy, ally)
+  def mate?(color)
+    check?(color) && find_all_legal_moves(color).none?
   end
 
-  def mate?(enemy, ally)
-    check?(enemy, ally) && !legal_moves_left?(ally)
+  def check?(color)
+    opposite_color = color == WHITE ? BLACK : WHITE
+    simulate_all_positions(find_same_pieces(opposite_color)).include?(find_king(color))
   end
 
-  def stalemate?(enemy, ally)
-    !check?(enemy, ally) && !legal_moves_left?(ally)
+  def stalemate?(color)
+    !check?(color) && find_all_legal_moves(color).none?
   end
 
   def empty?(position)
@@ -47,77 +66,49 @@ class Board
   end
 
   def inside?(position)
-    position[0] >= 0 && position[0] <= 7 && position[1] >= 0 && position[1] <= 7
+    position[0] >= 0 && position[0] < 8 && position[1] >= 0 && position[1] < 8
   end
 
-  def update_rqb_moves
-    @board.flatten.compact.each do |piece|
-      if piece.class.name == 'Rook'
-        piece.moves = piece.create_rook_moves
-      elsif piece.class.name == 'Bishop'
-        piece.moves = piece.create_bishop_moves
-      elsif piece.class.name == 'Queen'
-        piece.moves = piece.create_queen_moves
-      end
-    end
-  end
-
-  def move_piece(piece, move)
-    old_position = piece.position
-    new_position = piece.move(old_position, move)
-    @board[old_position[0]][old_position[1]] = EMPTY
-    @board[new_position[0]][new_position[1]] = piece
-  end
-
-  def castle(king, rook)
-    castling_possible?(king, rook)
-    king.position == king.initial_position && rook.position == rook.initial_position
-  end
-
-  def promotion_possible?(pawn)
-    pawn.color == 'White' ? pawn.position[0] == 0 : pawn.position[0] == 7
-  end
-
-  def promote(pawn, piece)
-    @board[pawn.position[0]][pawn.position[1]] = Object.const_get(piece).new(pawn.position, pawn.color)
+  def draw_board
+    @board.each { |_row| draw_row }
   end
 
   private
 
   def add_kings
-    add('King', [0, 4], 'Black')
-    add('King', [7, 4], 'White')
+    add('King', [0, 4], 'Black', PIECES[:b_king])
+    add('King', [7, 4], 'White', PIECES[:w_king])
   end
 
   def add_queens
-    add('Queen', [0, 3], 'Black')
-    add('Queen', [7, 3], 'White')
+    add('Queen', [0, 3], 'Black', PIECES[:b_queen])
+    add('Queen', [7, 3], 'White', PIECES[:w_queen])
   end
 
   def add_bishops
-    add('Bishop', [0, 2], 'Black')
-    add('Bishop', [0, 5], 'Black')
-    add('Bishop', [7, 2], 'White')
-    add('Bishop', [7, 5], 'White')
+    add('Bishop', [0, 2], 'Black', PIECES[:b_bishop])
+    add('Bishop', [0, 5], 'Black', PIECES[:b_bishop])
+    add('Bishop', [7, 2], 'White', PIECES[:w_bishop])
+    add('Bishop', [7, 5], 'White', PIECES[:w_bishop])
   end
 
   def add_knights
-    add('Knight', [0, 1], 'Black')
-    add('Knight', [0, 6], 'Black')
-    add('Knight', [7, 1], 'White')
-    add('Knight', [7, 6], 'White')
+    add('Knight', [0, 1], 'Black', PIECES[:b_knight])
+    add('Knight', [0, 6], 'Black', PIECES[:b_knight])
+    add('Knight', [7, 1], 'White', PIECES[:w_knight])
+    add('Knight', [7, 6], 'White', PIECES[:w_knight])
   end
 
   def add_rooks
-    add('Rook', [0, 0], 'Black')
-    add('Rook', [0, 7], 'Black')
-    add('Rook', [7, 0], 'White')
-    add('Rook', [7, 7], 'White')
+    add('Rook', [0, 0], 'Black', PIECES[:b_rook])
+    add('Rook', [0, 7], 'Black', PIECES[:b_rook])
+    add('Rook', [7, 0], 'White', PIECES[:w_rook])
+    add('Rook', [7, 7], 'White', PIECES[:w_rook])
   end
 
   def add_pawns
-    [*0..7].map { |i| add('Pawn', [1, i], 'Black') }
-    [*0..7].map { |i| add('Pawn', [6, i], 'White') }
+    [*0..7].map { |i| add('Pawn', [1, i], 'Black', PIECES[:b_pawn]) }
+    [*0..7].map { |i| add('Pawn', [6, i], 'White', PIECES[:w_pawn]) }
   end
 
   def add(piece, position, color)
@@ -125,38 +116,46 @@ class Board
   end
 
   def find_king(color)
-    @board.flatten.compact.select { |piece| piece.class.name == 'King' && piece.color == color}.first.position
+    @board.flatten.compact.select { |piece| piece.instance_of?(::King) && piece.color == color }.first.position
   end
 
-  def find_same_pieces(color)
+  def find_pieces(color)
     @board.flatten.compact.select { |piece| piece.color == color }
   end
 
   def simulate_all_positions(pieces)
-    pieces.map {|piece| make_fake_moves(piece) }.flatten(1)
+    pieces.map { |piece| piece.simulate_all_moves(pieces.moves) }.flatten(1)
   end
 
-  def make_fake_moves(piece)
-    piece.moves.map { |move| piece.move(piece.position, move) }
-  end
-
-  def legal_moves_left?(color)
-    return true unless find_legal_moves(color).empty?
-    false
-  end
-
-  def find_legal_moves(color)
-    legal_moves = []
-    ally = color
-    enemy = ally == 'White' ? 'Black' : 'White'
-    find_same_pieces(color).map do |piece|
-      piece.moves.map do |move|
-        fake_board = Board.new
-        fake_board.board = @board.map(&:dup)
-        fake_board.move_piece(piece, move)
-        legal_moves << move unless fake_board.check?(enemy, ally)
-      end
+  def find_all_legal_moves(color)
+    find_pieces(color).map do |piece|
+      check_legal_moves(piece)
     end
-    legal_moves
   end
+
+  def check_legal_moves(piece)
+    find_moves(piece).select do |move|
+      fake_board = Board.new
+      fake_board.board = @board.map(&:dup)
+      fake_board.move_piece(piece, move)
+      move unless fake_board.check?(color)
+    end
+  end
+
+  def find_moves(piece)
+    case piece
+    when instance_of?(::Rook)
+      piece.create_rook_moves(@board)
+    when instance_of?(::Bishop)
+      piece.create_bishop_moves(@board)
+    when instance_of?(::Queen)
+      piece.create_queen_moves(@board)
+    when instance_of?(::Pawn)
+      piece.create_moves
+    else
+      piece.moves
+    end
+  end
+
+  def draw_row; end
 end
