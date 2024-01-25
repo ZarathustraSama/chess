936a1ccd2af7e8d47824e7a10360967f348042b1
ZarathustraSama <mr.gabriel@live.it>
2024-01-25 10:21:35 +0100

    Implemented all tests for class methods

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -3,12 +3,21 @@
 require_relative '../lib/piece'
 
 describe Piece do
+  subject(:piece) { described_class.new([0, 0], 'Black', '\u265C') }
+
   describe '#move' do
-    subject(:piece) { described_class.new([0, 0], 'Black', '\u265C') }
     let(:move) { [3, 0] }
 
     it 'returns the new position of the piece' do
       expect(piece.move(move)).to eql([3, 0])
+    end
+  end
+
+  describe '#simulate_all_moves' do
+    let(:moves) { [[1, 0], [2, 0], [3, 0]] }
+
+    it 'returns an array of all simulated positions' do
+      expect(piece.simulate_all_moves(moves)).to eql([[1, 0], [2, 0], [3, 0]])
     end
   end
 end
