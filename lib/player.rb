d34038ed1daa92e73b51d191dffc4fd55f46994f
ZarathustraSama <mr.gabriel@live.it>
2024-01-25 10:03:15 +0100

    Resetted class for later

--- lib/player.rb
+++ lib/player.rb
@@ -1,21 +1,6 @@
 # frozen_string_literal: true
 
+
 class Player
-  attr_accessor :player
 
-  def initialize
-    @player = nil
-  end
-
-  def set_player(color)
-    return 'White' if @player.nil?
-
-    @player.white? ? 'Black' : 'White'
-  end
-
-  private
-
-  def white?
-    @player == 'White'
-  end
 end
