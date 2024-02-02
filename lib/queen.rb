093a2705545ad2ee91f6b0055b00ac3754000af1
ZarathustraSama <mr.gabriel@live.it>
2024-02-02 11:32:46 +0100

    Queen is now responsible for generating its own moves

--- lib/queen.rb
+++ lib/queen.rb
@@ -1,19 +1,54 @@
 # frozen_string_literal: true
 
 require_relative './piece'
-require_relative './move'
 
 # The queen piece
 class Queen < Piece
-  attr_accessor :moves
-
-  def initialize(*args)
-    super
-    @moves = []
+  def generate_legal_moves(board)
+    @moves = queen_moves(board)
   end
 
   def queen_moves(board)
-    m = Move.new
-    m.x_moves(board) + m.y_moves(board) + m.positive_xy_moves(board) + m.negative_xy_moves(board)
+    rook_moves(board) + bishop_moves(board)
+  end
+
+  def bishop_moves(board)
+    first_quadrant(board) + second_quadrant(board) + third_quadrant(board) + fourth_quadrant(board)
+  end
+
+  def rook_moves(board)
+    vertical_positive(board) + vertical_negative(board) + horizontal_positive(board) + horizontal_negative(board)
+  end
+
+  def first_quadrant(board)
+    create_moves(board, [-1, 1])
+  end
+
+  def second_quadrant(board)
+    create_moves(board, [-1, -1])
+  end
+
+  def third_quadrant(board)
+    create_moves(board, [1, -1])
+  end
+
+  def fourth_quadrant(board)
+    create_moves(board, [1, 1])
+  end
+
+  def vertical_positive(board)
+    create_moves(board, [-1, 0])
+  end
+
+  def vertical_negative(board)
+    create_moves(board, [1, 0])
+  end
+
+  def horizontal_positive(board)
+    create_moves(board, [0, 1])
+  end
+
+  def horizontal_negative(board)
+    create_moves(board, [0, -1])
   end
 end
