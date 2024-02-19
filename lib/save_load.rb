7bb77bbc4364c2fbe7860091f898bc24838ca97f
ZarathustraSama <mr.gabriel@live.it>
2024-02-19 11:40:05 +0100

    Moved all save/load related methods here instead

--- lib/save_load.rb
+++ lib/save_load.rb
@@ -4,6 +4,36 @@ require 'json'
 require_relative './game'
 require_relative './board'
 
+# Methods for saving/loading the game
+def save_game(game)
+  Dir.mkdir('saves') unless Dir.exist?('saves')
+
+  File.open('saves/game.json', 'w') do |file|
+    file.puts game_to_json(game)
+  end
+  puts 'Game has been saved'
+end
+
+def load_game
+  puts 'Now loading...'
+  File.open('saves/game.json', 'r') do |file|
+    game_from_json(file.gets)
+  end
+end
+
+# check for saved game and ask user if they want to use it
+def try_loading_game
+  load_game if saved_game? && ask_user_load
+end
+
+def saved_game?
+  begin
+    !Dir.empty?('saves')
+  rescue Errno::ENOENT
+    false
+  end
+end
+
 def game_to_json(game)
   JSON.dump ({
     :board => board_to_json(game.board.board),
@@ -36,26 +66,4 @@ def load_board(saved_board)
     board.add(p['piece'], p['position'], p['color'], p['moved'], p['moves'])
   end
   board
-end
-
-def save_game(game)
-  Dir.mkdir('saves') unless Dir.exist?('saves')
-
-  File.open('saves/game.json', 'w') do |file|
-    file.puts game_to_json(game)
-  end
-end
-
-def saved_game?
-  begin
-    !Dir.empty?('saves')
-  rescue Errno::ENOENT
-    false
-  end
-end
-
-def load_game
-  File.open('saves/game.json', 'r') do |file|
-    game_from_json(file.gets)
-  end
 end
