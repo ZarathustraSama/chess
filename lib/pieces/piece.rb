e6e8c7a462bc26037c475c428411e06b71f33e01
ZarathustraSama <mr.gabriel@live.it>
2024-02-20 11:26:04 +0100

    Cleaned some useless variables

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -2,23 +2,18 @@
 
 # Every component of the board will inherit this class
 class Piece
-  attr_reader :color, :initial_position, :symbol
-  attr_accessor :position, :moves, :moved
+  attr_reader :color, :moved
+  attr_accessor :position, :moves
 
   def initialize(position, color, moved, moves)
     @position = position
     @color = color
-    @moved = false
+    @moved = moved
     @moves = moves
-    @symbol = nil
   end
 
   def move(move, position = @position)
     [position[0] + move[0], position[1] + move[1]]
-  end
-
-  def moved!
-    @moved = true
   end
 
   def legal_move?(move, player)
