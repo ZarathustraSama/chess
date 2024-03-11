ebb103d48a3567dca536e765e2c3d9d983a9a03b
ZarathustraSama <mr.gabriel@live.it>
2024-03-11 09:34:46 +0100

    Cleaned some code, game cycle now doesn't break

--- lib/play.rb
+++ lib/play.rb
@@ -5,37 +5,35 @@ require_relative './utility'
 require_relative './save_load'
 
 def play
-  game = try_loading_game
-
+  clean_terminal
+  game = load_game
   if game.nil?
     game = Game.new
     game.initial_state
   end
-
-  board = game.board # The board object
-  checkboard = board.board # The 2D array reppresentation of the board
-  player = game.player
-  game.draw_board(checkboard)
-
+  board = game.board
+  game.draw_board
+  greet_prompt
   while true
-    game.update_state
-    return if game_over?(board, player)
+    player = game.player
+    exit if game_over?(board, player)
 
-    check_prompt if board.check?(player)
+    check_prompt(player) if board.check?(player)
     puts "#{player} moves"
     input = ask_user_input(game)
-
     piece, move = board.find_piece(input[0]), input[1]
-    if !piece.nil? && game.legal_move?(piece, move, player)
-      board.move_piece(piece, move)
-      game.promote(piece, ask_user_promotion_piece) if piece&:can_promote?
-      game.update_state
-      game.draw_board(checkboard)
-      player = game.player
+    if game.legal_move?(piece, move, player)
+      game.update_state(piece, move)
+      clean_terminal
+      game.draw_board
     else
       illegal_move_prompt
     end
   end
+end
+
+def clean_terminal
+  system "clear" || "cls"
 end
 
 def game_over?(board, player)
@@ -50,16 +48,3 @@ def game_over?(board, player)
     end
   end
 end
-
-  # start loop
-  #   ask user input
-  #   if wrong format, repeat
-  #   check piece, check moves
-  #   if move is not one of the technically possible moves, repeat
-  #   if fake board causes a check for the player, repeat
-  #   modify board
-  #   check for checks, mates, stalemate for the other player
-  #   check for promotion if piece is a pawn
-  #   close game if game over
-  #   change player
-  # end loop
