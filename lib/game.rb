739f954b6e4a52a661366853ef314bd8e295f685
ZarathustraSama <mr.gabriel@live.it>
2024-03-11 09:34:03 +0100

    Game can handle pawn logic and validity of moves

--- lib/game.rb
+++ lib/game.rb
@@ -4,28 +4,77 @@ require_relative './board'
 
 # This is where interaction of the pieces and changes to the board takes place
 class Game
-  attr_accessor :board, :player
+  attr_accessor :board, :player, :moves_to_draw, :double_step_pawn
 
-  def initialize(board = Board.new, player = nil, moves_to_draw = 0)
+  def initialize(board = Board.new, player = WHITE, moves_to_draw = 0, double_step_pawn = nil)
     @board = board
     @player = player
     @moves_to_draw = moves_to_draw
+    @double_step_pawn = double_step_pawn
   end
 
   def initial_state
     @board.populate_board
-    set_player
     @board.update_moves(@player)
   end
 
-  def update_state
+  def update_state(piece, move)
+    update_counter(piece, move)
+    update_pawn(board.move_piece(piece, move), move)
+    @double_step_pawn = nil
     set_player
     @board.update_moves(@player)
+    @board.find_piece(@double_step_pawn) unless @double_step_pawn.nil?
+  end
+
+  def update_pawn(pawn, move)
+    return unless pawn.instance_of?(::Pawn)
+
+    @double_step_pawn = pawn.position if pawn.double_step?(move[0])
+    @board.double_step_pawn = @double_step_pawn
+    promote(pawn)
+  end
+
+  def update_counter(piece, move)
+    unless piece.instance_of?(::Pawn) && @board.empty?(move)
+      @moves_to_draw += 1
+    else
+      @moves_to_draw = 0
+    end
   end
 
   # The move has to be in the set of moves of the piece, and it should not cause a self-check
+  # Capturing the king is also not allowed
   def legal_move?(piece, move, player)
-    piece.legal_move?(move, player) && !next_check?(piece, move, player)
+    return if piece.nil?
+
+    square = @board.board[move[0]][move[1]]
+    piece.legal_move?(move, player) && !square.instance_of?(::King) && !next_check?(piece, move, player)
+  end
+
+  def can_claim_draw?
+    @moves_to_draw >= 50 # || repetition of 3 moves
+  end
+
+  def promote(pawn)
+    return unless pawn.can_promote?
+
+    piece = ask_user_promotion_piece
+    @board.board[pawn.position[0]][pawn.position[1]] = Object.const_get(piece).new(pawn.position, pawn.color, false, [])
+  end
+
+  def draw_board(board = @board.board)
+    puts ''
+    i = 9
+    board.each { |row| draw_row(row, i -= 1) }
+    puts '   a b c d e f g h'
+    puts ''
+  end
+
+  private
+
+  def set_player
+    @player = @player == WHITE ? BLACK : WHITE
   end
 
   # Simulates a new board and verifies if a check occurs
@@ -33,42 +82,8 @@ class Game
     @board.simulate_new_board(piece, move).check?(player)
   end
 
-  def can_claim_draw?
-    @moves_to_draw >= 50 # || repetition of 3 moves
-  end
-
-  def promote(pawn, piece)
-    @board.board[pawn.position[0]][pawn.position[1]] = Object.const_get(piece).new(pawn.position, pawn.color)
-  end
-
-  def update_counter(piece, move)
-    unless piece.instance_of(::Pawn) && @board.empty?(move)
-      @moves_to_draw += 1
-    else
-      @moves_to_draw = 0
-    end
-  end
-
-  def draw_board(board = @board.board)
-    puts ''
-    board.each { |row| draw_row(row) }
-    puts ''
-  end
-
-  private
-
-  def set_player
-    case @player
-    when WHITE
-      @player = BLACK
-    when BLACK
-      @player = WHITE
-    else
-      @player = WHITE
-    end
-  end
-
-  def draw_row(row)
+  def draw_row(row, i)
+    print "#{i} "
     row.each do |square|
       if square.nil?
         print '|_'
